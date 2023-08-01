17eda6dc9cde66ea9a780fecfa855f09c6385fbe
sriveer-neerukonda <sriveer.neerukonda@hotmail.com>
2023-08-01 08:24:47 +0530

    feat:initialised the project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App(){
+    return (
+        <>
+            <h1>Where in the world?</h1>
+            <h2>Belgium</h2>
+            <h3 className="body-text"><b>Capital:</b> Brussels</h3>
+        </>
+    );
+}
