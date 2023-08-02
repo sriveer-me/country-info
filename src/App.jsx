028cb30017a58314c76fb0edb3fb33f753f0cff2
sriveer-neerukonda <sriveer.neerukonda@hotmail.com>
2023-08-02 18:36:45 +0530

    feat:ability to change app theme from light to dark and vice versa on demand

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 export default function App(){
+
+    function toggleTheme(){
+        document.documentElement.classList.toggle('dark-mode');
+    }
+
     return (
         <>
             <h1>Where in the world?</h1>
             <h2>Belgium</h2>
             <h3 className="body-text"><b>Capital:</b> Brussels</h3>
+            <button onClick={toggleTheme}>Change Theme</button>
         </>
     );
 }
